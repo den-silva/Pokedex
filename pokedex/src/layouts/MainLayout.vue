b48b8eb6d79f21f71122ea26dbc3b82ba924d927
den-silva <dmurilosilva@hotmail.com>
2021-07-05 14:09:38 -0300

    Instalação e configuração do projeto

---
+++ pokedex/src/layouts/MainLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+
+        <q-toolbar-title class="text-center">
+          Pokedex
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  }
+})
+</script>
