4200d41687c52c94a58c4c8fead0fcb516b544da
Denilson <dmurilosilva@hotmail.com>
2021-07-06 08:21:59 -0300

    Instalação do Axios, configuração e consumo da API

--- pokedex/src/pages/Index.vue
+++ pokedex/src/pages/Index.vue
@@ -5,9 +5,36 @@
 </template>
 
 <script>
+  import api from '../services/api.js'
+
+  export default {
+    name: "PageIndex",
+
+    beforeMount() {
+    // Make a request for a user with a given ID
+    api.get('pokemon/157/')
+      .then( (response) => {
+        // handle success
+        console.log(response);
+      })
+      .catch( (error) =>{
+        // handle error
+        console.log(error);
+      })
+      .then( () =>{
+        // always executed
+      });
+
+
+  }
+}  
+
 import { defineComponent } from 'vue';
 
-export default defineComponent({
-  name: 'PageIndex'
-})
+
+
+
+
+
+
 </script>
